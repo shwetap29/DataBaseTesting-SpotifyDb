20f0a6f7180b0d4c98a439d4c1bd4c654a8f0902
shwetap29 <shwetaprasad48@gmail.com>
2021-08-02 15:18:57 +0530

    ADDED:Worked with Update, delete, orderby query

--- src/test/java/com/bridgelabz/selenium/DBTesting.java
+++ src/test/java/com/bridgelabz/selenium/DBTesting.java
@@ -8,6 +8,8 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class DBTesting  extends Base {
+    private int count;
+
     @Test
     public void get_table_data() throws ClassNotFoundException, SQLException {
         con = this.getConnection();
@@ -20,6 +22,7 @@ public class DBTesting  extends Base {
             String name = rs.getString(2);
             String emailid = rs.getString(3);
             System.out.println(id + " " + name + " " + emailid);
+            count++;
         }
     }
 
@@ -32,6 +35,33 @@ public class DBTesting  extends Base {
         pst.setString(3, "rakeshprasad55@gmail.com");
         pst.executeUpdate();
     }
+    @Test
+    public void update_values_into_table() throws ClassNotFoundException, SQLException {
+        con = this.getConnection();
+        PreparedStatement pst = con.prepareStatement("UPDATE tracks SET playlist=? WHERE track=?");
+                pst.setString(1,"Fusion");
+                pst.executeUpdate();
+
+    }
+
+    @Test
+    public void delete_row_from_table() throws ClassNotFoundException, SQLException {
+        con = this.getConnection();
+        PreparedStatement pst=con.prepareStatement("DELETE FROM tracks WHERE track= ?");
+        pst.setString(1,"Rishtey");
+        pst.executeUpdate();
+        get_table_data();
+    }
+
+    @Test
+    public void order_by_playlistName() throws ClassNotFoundException, SQLException {
+        con = this.getConnection();
+        PreparedStatement pst=con.prepareStatement("SELECT * FROM playlist ORDER BY playlistName");
+        pst.setInt(1,6);
+        pst.setString(2,"Bollywood");
+        pst.executeUpdate();
+    }
 }
+
 
 
